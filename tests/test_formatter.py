d05f239902496b0533c9242460d1a00ebf995ac7
Robert Roos <rroos@highvolteng.com>
2026-01-22 09:37:14 +0100

    Reformated tests based on new black version

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -23,13 +23,11 @@ def test_help(capsys):
 def test_cli(plc_code, capsys):
     """Test the CLI hook works."""
     config = plc_code / "TwinCAT Project1" / ".editorconfig"
-    config.write_text(
-        """root = true
+    config.write_text("""root = true
 [*.TcPOU]
 indent_style = space
 indent_size = 4
-"""
-    )
+""")
     file = plc_code / "TwinCAT Project1" / "MyPlc" / "POUs" / "FB_Example.TcPOU"
 
     path = sys.executable  # Re-use whatever executable we're using now
@@ -44,13 +42,11 @@ indent_size = 4
 def test_dry_no_tab_char(plc_code, caplog):
     """Test finding illegal tab characters."""
     config = plc_code / "TwinCAT Project1" / ".editorconfig"
-    config.write_text(
-        """root = true
+    config.write_text("""root = true
 [*.TcPOU]
 indent_style = space
 indent_size = 4
-"""
-    )
+""")
     file = plc_code / "TwinCAT Project1" / "MyPlc" / "POUs" / "FB_Example.TcPOU"
     content_before = file.read_text()
 
@@ -78,13 +74,11 @@ indent_size = 4
 def test_dry_tab_spaces(plc_code, caplog):
     """Test finding illegal spaces."""
     config = plc_code / "TwinCAT Project1" / ".editorconfig"
-    config.write_text(
-        """root = true
+    config.write_text("""root = true
 [*.TcPOU]
 indent_style = tab
 indent_size = 4
-"""
-    )
+""")
     file = plc_code / "TwinCAT Project1" / "MyPlc" / "POUs" / "FB_Example.TcPOU"
 
     formatter = Formatter(str(file), "--dry", "-l", "DEBUG")
@@ -110,12 +104,10 @@ indent_size = 4
 def test_dry_trailing_ws(plc_code, caplog):
     """Test finding illegal ws."""
     config = plc_code / "TwinCAT Project1" / ".editorconfig"
-    config.write_text(
-        """root = true
+    config.write_text("""root = true
 [*.TcPOU]
 trim_trailing_whitespace = true
-"""
-    )
+""")
     file = plc_code / "TwinCAT Project1" / "MyPlc" / "POUs" / "FB_Example.TcPOU"
 
     formatter = Formatter(str(file), "--dry", "-l", "DEBUG")
@@ -138,13 +130,11 @@ trim_trailing_whitespace = true
 def test_check(plc_code, caplog):
     """Test `check` flag for formatter."""
     config = plc_code / "TwinCAT Project1" / ".editorconfig"
-    config.write_text(
-        """root = true
+    config.write_text("""root = true
 [*.TcPOU]
 indent_style = space
 indent_size = 4
-"""
-    )
+""")
     file = plc_code / "TwinCAT Project1" / "MyPlc" / "POUs" / "FB_Example.TcPOU"
 
     content_before = file.read_text()
@@ -165,13 +155,11 @@ def test_check_recursive(plc_code, caplog):
     """Test `check` flag for formatter, on a folder"""
     project_folder = plc_code / "TwinCAT Project1"
     config = project_folder / ".editorconfig"
-    config.write_text(
-        """root = true
+    config.write_text("""root = true
 [*.TcPOU]
 indent_style = space
 indent_size = 4
-"""
-    )
+""")
     formatter = Formatter(str(project_folder), "--check", "-r", "-l", "DEBUG")
     code = formatter.run()
 
@@ -213,13 +201,11 @@ def test_reformat_empty_config(plc_code):
 def test_reformat_no_tab_char(plc_code):
     """Test reformatting for illegal tab characters."""
     config = plc_code / "TwinCAT Project1" / ".editorconfig"
-    config.write_text(
-        """root = true
+    config.write_text("""root = true
 [*.TcPOU]
 indent_style = space
 indent_size = 4
-"""
-    )
+""")
     file = plc_code / "TwinCAT Project1" / "MyPlc" / "POUs" / "FB_Example.TcPOU"
 
     formatter = Formatter(str(file))
@@ -232,16 +218,14 @@ indent_size = 4
 def test_reformat_everything(plc_code):
     """Test reformatting for a typical file with a bunch of stuff."""
     config = plc_code / "TwinCAT Project1" / ".editorconfig"
-    config.write_text(
-        """root = true
+    config.write_text("""root = true
 [*.TcPOU]
 indent_style = space
 indent_size = 4
 trim_trailing_whitespace = true
 insert_final_newline = true
 twincat_align_variables = true
-"""
-    )
+""")
     file = plc_code / "TwinCAT Project1" / "MyPlc" / "POUs" / "FB_Full.TcPOU"
 
     formatter = Formatter(str(file))
@@ -262,12 +246,10 @@ def test_reformat_eol(plc_code, eol):
     config_eol, write_eol, expected_eol = eol
 
     config = plc_code / "TwinCAT Project1" / ".editorconfig"
-    config.write_text(
-        f"""root = true
+    config.write_text(f"""root = true
     [*.TcPOU]
     end_of_line = {config_eol}
-    """
-    )
+    """)
 
     # Version control will affect line endings, so just make the file here
     content_list = [
